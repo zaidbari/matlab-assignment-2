7ad0d7d362c3286a2ce4f84fb101ee8c11cac8e6
Zaid Bari <zaidbari9@hotmail.com>
2023-01-02 16:25:29 +0300

    Update calculateDerivative.m

--- calculateDerivative.m
+++ calculateDerivative.m
@@ -1,12 +1,12 @@
 % Callback function for calculate derivative button
 function calculateDerivative(hObject, eventdata)
 	% Calculate derivative of function
-    syms x
+    	syms x
 
 	function_string = exp(x)*cos(x);
 	derivative = diff(diff(function_string)); % Calculate derivative
 
 	% Display derivative in message box
-    msg = sprintf('The 2nd derivative of the function is: %s\n', derivative);
-    msgbox(msg);
+    	msg = sprintf('The 2nd derivative of the function (exp(x)*cos(x)) is: %s\n', derivative);
+   	 msgbox(msg);
 end
